50a991acb7559600292c77c8fc0d12a7832c8b22
Gino Luiggi Chirinos Plasencia <luiggiplasencia0823@gmail.com>
2019-05-25 00:00:31 +0200

    Agregando contenido de filtros por defecto

--- app/app.js
+++ app/app.js
@@ -17,6 +17,14 @@
             {name:'Julie', phone:'555-8765'},
             {name:'Juliette', phone:'555-5678'}
         ];
+
+        $scope.number = 1234.56789;
+
+        $scope.name = 'Gino Luiggi';
+
+        $scope.reverse = false;
+
+        $scope.numbers = [1,2,3,4,5,6,7,8,9];
     }])
     .directive('appNavbar', [function () {
         return {
